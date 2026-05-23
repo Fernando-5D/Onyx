23bbecb0f4e94d299be499c2eec3d084da544f10
EdgarUrielMoralesTorres <23308060610613@cetis61.edu.mx>
2026-05-23 15:52:15 -0600

    Create PaginaModel.py

---
+++ src/models/PaginaModel.py
@@ -0,0 +1,79 @@
+import mysql.connector
+from models.Database import Database
+
+class PaginaModel:
+    def __init__(self, db: Database):
+        self.db = db
+
+    def crear(self, id_usuario, nombre, contenido):
+        try:
+            conn = self.db.get_connection()
+            cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                INSERT INTO paginas (id_usuario, nombre, contenido)
+                VALUES (%s, %s, %s)
+                """,
+                (id_usuario, nombre, contenido)
+            )
+
+            conn.commit()
+            return True, "Página creada correctamente"
+
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return False, "Hubo un error al crear"
+
+        finally:
+            cursor.close()
+            conn.close()
+
+
+    def editar(self, id_pagina, nombre, contenido):
+        try:
+            conn = self.db.get_connection()
+            cursor = conn.cursor()
+
+            cursor.execute(
+                """
+                UPDATE paginas
+                SET nombre = %s,
+                    contenido = %s
+                WHERE id = %s
+                """,
+                (nombre, contenido, id_pagina)
+            )
+
+            conn.commit()
+            return True, "Actualizada correctamente"
+
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return False, "Hubo un error al editar"
+
+        finally:
+            cursor.close()
+            conn.close()
+
+
+    def eliminar(self, id_pagina):
+        try:
+            conn = self.db.get_connection()
+            cursor = conn.cursor()
+
+            cursor.execute(
+                "DELETE FROM paginas WHERE id = %s",
+                (id_pagina,)
+            )
+
+            conn.commit()
+            return True, "Eliminada correctamente"
+
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return False, "Hubo un error al eliminar"
+
+        finally:
+            cursor.close()
+            conn.close()
